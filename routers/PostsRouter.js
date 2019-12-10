1101ed994e886ff7d09ae544dcf1cea3e1a784da
Tara Sherman <taradelin@gmail.com>
2019-12-10 17:26:03 -0500

    Successfully adding a post

--- routers/PostsRouter.js
+++ routers/PostsRouter.js
@@ -84,5 +84,27 @@ router.get('/:id', (req, res) => {
     };
 });
 
+// add a post
+router.post('/', (req, res) => {
+  const { title, contents } = req.body;
+
+  if (!title || !contents) {
+    return res.status(400).json({
+      errorMessage: "Please provide title and contents for the post."
+    })
+  } else {
+    posts.insert(req.body)
+    .then(post => {
+      res.status(201).json(req.body)
+    })
+    .catch(err => {
+      return res.status(500).json({
+        error: "There was an error while saving the post to the database"
+      })
+    })
+  };
+});
+
+
 
 module.exports = router;
